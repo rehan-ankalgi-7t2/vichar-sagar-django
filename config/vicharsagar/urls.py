8968daa352d4befdf065715b968d4ea78822ed18
rehan-ankalgi-7t2 <rehanankalgi7t2@gmail.com>
2024-08-29 20:45:21 +0530

    authentication login, register, log out functionality complete

--- config/vicharsagar/urls.py
+++ config/vicharsagar/urls.py
@@ -20,6 +20,10 @@ from . import views
 
 urlpatterns = [
     path("", views.home_view, name="home"),
-    path("login/", views.login, name="login"),
-    path("register/", views.register, name="register")
+    path("login/", views.login_view, name="login"),
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile_view, name="profile"),
+    path("about/", views.about_view, name="about"),
+    path("contact/", views.contact_view, name="contact"),
+    path("notifications/", views.notifications_view, name="notifications"),
 ]
--- config/vicharsagar/views.py
+++ config/vicharsagar/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from .models import Article, Topic
+from django.shortcuts import render, redirect
+import re
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
+from .models import Article, Topic
 
 # Create your views here.
 def home_view(request):
@@ -21,22 +24,55 @@ def profile_view(request):
 def list_view(request):
     return render(request, "vicharsagar/lists.html")
 
-def login(request):
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        existing_user = User.objects.filter(username = username)
+        if not  existing_user.exists():
+            context = {
+                "error_message": "User does not exist, please register"
+                }
+            return render(request, "vicharsagar/login.html", context)
+        
+        authenticated_user = authenticate(username=username, password=password)
+
+        if authenticated_user is not None:
+            login(request, authenticated_user)
+            return redirect("home")
+            
     return render(request, "vicharsagar/login.html")
 
 def register(request):
+    # TODO: explore django messages
     if request.method == "POST":
         first_name = request.POST.get("first_name")
         last_name = request.POST.get("last_name")
         username = request.POST.get("username")
         password = request.POST.get("password")
+        email_address = request.POST.get("email_address")
+
+        # validate email using regex
+        email_regex = r"^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9]+$"
+        if not re.match(email_regex, email_address):
+            return render(request, "vicharsagar/register.html", {"error_message": "Invalid email address."})
 
         try:
+            # check if the user already exists
+            existing_user = User.objects.filter(username = username)
+            if existing_user.exists():
+                context = {
+                    "error_message": "User already exists, please log in"
+                }
+                return render(request, "vicharsagar/register.html", context)
+
             # Attempting to create  a new user
             new_user = User.objects.create_user(
                 first_name = first_name,
                 last_name = last_name,
                 username = username,
+                email = email_address,
                 password = password
             )
             new_user.save()
@@ -67,3 +103,19 @@ def register(request):
 
 
     return render(request, "vicharsagar/register.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+def about_view(request):
+    return render(request, "vicharsagar/about.html")
+
+def contact_view(request):
+    return render(request, "vicharsagar/about.html")
+
+def notifications_view(request):
+    return render(request, "vicharsagar/about.html",)
+
+def profile_view(request):
+    return render(request, "vicharsagar/profile.html")
