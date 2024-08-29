766c49f0f85f0e5b0b78ed4bdc2af055c29db4d5
rehan-ankalgi-7t2 <rehanankalgi7t2@gmail.com>
2024-08-29 17:29:21 +0530

    register action complete

--- config/vicharsagar/views.py
+++ config/vicharsagar/views.py
@@ -25,4 +25,45 @@ def login(request):
     return render(request, "vicharsagar/login.html")
 
 def register(request):
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            # Attempting to create  a new user
+            new_user = User.objects.create_user(
+                first_name = first_name,
+                last_name = last_name,
+                username = username,
+                password = password
+            )
+            new_user.save()
+
+            context = {
+                "success_message": "Registration successfull! Please log in."
+            }
+
+            return render(request, "vicharsagar/login.html", context)
+        
+        # handles db integrity errors like, already existing record
+        except IntegrityError as e:
+            if "username" in str(e):
+                context = {
+                    "error_message": "Username already exists."
+                }
+            else:
+                context = {
+                    "error_message": "Email already in use."
+                }
+            return render(request, "vicharsagar/register.html", context)
+        # catch any other unexpected error / exceptions
+        except Exception as e:
+            context = {
+                "error_message": "An error occurred during registration. Please try again."
+                }
+            return render(request, "vicharsagar/register.html", context)
+
+
     return render(request, "vicharsagar/register.html")
